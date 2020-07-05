0dfb7236e0e5ed192e2045ffae777410871530e8
Glitch (chaosgame) <none>
2020-07-05 21:35:04 +0000

    Cleanup and names

--- client.js
+++ client.js
@@ -18,11 +18,63 @@ new p5() // including this lets you use p5's globals everywhere
 
 const infoh = 26 // how many pixels high the info lines at the bottom are
 
-const nom = {
-  "sierpinski triangle": [3, 0, 6],
-  "chinese checkers":    [6, 3, 6],
-  "DUD":                 [],
-}
+const nom = [
+// name, [noa, hub, cac, pat], stars
+["sierpinski triangle", [3, 0, 0, 6], 5],
+["chinese checkers",    [6, 0, 3, 6], 5],
+["DUD",                 [],           0],
+]
+
+// -----------------------------------------------------------------------------
+// Cool settings (noa, cac, pat):
+// 3, 0, 1/2  -- plain sierpinski triangle
+// 6, 3, 1/2  -- chinese checkers board
+// 6, 2, 1/2  -- ninja star
+// 5, 1, 1/2  -- day of the dead
+// 6, 0, 1/2  -- stained glass window
+// 6, 3, 1/3  -- snowflake
+// 4, 1, 1/2  -- squares
+// 12, 6, 1/2 -- lightning donut
+// 5, 3, 1/3  -- pentagon star
+// 7, 3, 1/2  -- asterisk
+// 5, 0, 1/2  -- holy pentagon
+// 7, 4, 1/2  -- snowflakey flower
+// 3, 1, 1/3  -- arrows
+// 9, 4, 1/2  -- jaggly nonagon
+// 7, 1, 2/3  -- lego hands
+// 10, 2, 1/2 -- fluffy well
+// 6, 0, 7/12 -- stars of david
+// 12, 2, 3/4 -- lego feet
+// 8, 0, 2/3  -- coral 
+// 10, 0, 2/3 -- footprints
+// 9, 6, 1/3  -- sun
+// 9, 0, 2/3  -- bubbles
+// 5, 0, 7/12 -- pentaflake
+// 4, 2, 1/4  -- pinwheel squares
+// 3, 0, 1/4  -- fuzzy triangle
+// 5, 0, 1/3  -- warm pentagon
+// 8, 2, 2/3  -- tendrils
+// 8, 0, 7/12 -- figure eights
+// 9, 6, 1/4  -- table saw
+// 7, 3, 5/12 -- lobster
+// 5, 2, 1/4  -- spokey
+// 5, 1, 1/3  -- pentaspoke
+// 3, 1, 1/4  -- hippo
+// 9, 3, 1/2  -- fire breathing lizards
+// 8, 2, 7/12 -- gearflake
+// 12, 3, 2/3 -- ragdolls
+//https://chaosgame.glitch.me/?noa=11&hub=0&cac=2&pat=8 -- dumbo
+//https://chaosgame.glitch.me/?noa=6&hub=0&cac=0&pat=8 -- kochflake
+//https://chaosgame.glitch.me/?noa=6&hub=1&cac=0&pat=6 -- pentafluff
+//https://chaosgame.glitch.me/?noa=8&hub=1&cac=0&pat=8 -- flowerpower or daisychain
+//https://chaosgame.glitch.me/?noa=7&hub=1&cac=0&pat=8 -- snowflower
+//https://chaosgame.glitch.me/?noa=4&hub=0&cac=1&pat=5 -- carpet
+//https://chaosgame.glitch.me/?noa=9&hub=1&cac=0&pat=8 -- wheel
+//https://chaosgame.glitch.me/?noa=4&hub=1&cac=0&pat=6 -- sierpinski dribble
+// -----------------------------------------------------------------------------
+// REJECT PILE:
+// 6, 4, 1/2  -- molecule
+
 
 // Number Of Attractors
 let noa = parseInt(getQueryParam('noa', randrange(3, 12)))
@@ -232,14 +284,10 @@ function draw() {
       for (let ry = 0; ry < height; ry++) {
         n = grid[rx][ry]
         if (n > 0) {
-          noFill()
-          stroke(blink(1), 1, 1)
           if      (calg === 0) { stroke(1, 0, .3) } 
           else if (calg === 1) { stroke(blink((n-minp)/(maxp-minp)), 1, 1) }
           else if (calg <= 8)  { stroke(blink(tah[n]), 1, 1) }
-          // Baffling bug here where it redraws with all one color sometimes
           point(rx, ry)
-          stroke(1, 0, .5) // debugging
         }
       }
     }
